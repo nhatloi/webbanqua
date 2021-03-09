a1f358ab1a63b98189d85be067f6eb4b911676aa
nhatloi <1724801030179@student.tdmu.edu.vn>
2021-03-09 08:06:52 +0700

    Update captcha.php

--- include/taikhoan/captcha.php
+++ include/taikhoan/captcha.php
@@ -4,10 +4,6 @@ $string = md5(time());
 $string = substr($string, 0, 6);
  
 $_SESSION['maxacnhan'] = $string;
-<<<<<<< HEAD
- 
-=======
->>>>>>> f3545bf050a12679f42b5db0cd7497475bf4ecb8
 $img = imagecreate(150,50);
 $background = imagecolorallocate($img, 0,0,0);
 $text_color = imagecolorallocate($img, 255,255,255);
