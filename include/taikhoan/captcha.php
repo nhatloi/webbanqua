eafb74298981464c2730a88367ebec5d2e9c552e
nhatloi <75830060+nhatloi@users.noreply.github.com>
2021-03-09 09:13:20 +0700

    Update captcha.php

--- include/taikhoan/captcha.php
+++ include/taikhoan/captcha.php
@@ -7,7 +7,7 @@ $_SESSION['maxacnhan'] = $string;
 $img = imagecreate(150,50);
 $background = imagecolorallocate($img, 0,0,0);
 $text_color = imagecolorallocate($img, 255,255,255);
-imagestring($img, 4,40,15, $string, $text_color);
+//imagestring($img, 4,40,15, $string, $text_color);
  
 header("Content-type: image/png");
 imagepng($img);
