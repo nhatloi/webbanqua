99ea6522ee0cf9cad5fe2544f86587ea43ed7f34
nhatloi <75830060+nhatloi@users.noreply.github.com>
2021-07-08 00:49:09 +0700

    Update ketnoi.php

--- include/ketnoi.php
+++ include/ketnoi.php
@@ -6,6 +6,6 @@ $userName = 'a771dc_dtdm2';
 //khai báo biến password
 $passWord = '1q2w3e4r';
 // khai báo biến databaseName
-$databaseName = 'db_a771dc_dtdm';
+$databaseName = 'db_a771dc_dtdm2';
 $conn = mysqli_connect($hostName, $userName, $passWord , $databaseName) or die ('Không thể kết nối tới database');
 ?>
